bff50db6e67545bf58f54041269b5d99f4423d25
João Pedro de Paula Pannain Souza <joaopedro@ultra.pro.br>
2018-06-22 00:00:01 -0300

    Informativos iniciais.

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function HelloWorld() {
+    console.log('TESTE');
+}
+exports.HelloWorld = HelloWorld;
---
+++ lib/index.ts
@@ -0,0 +1,6 @@
+
+
+
+export function HelloWorld(){
+    console.log('TESTE');
+}
