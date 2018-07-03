6d91d5783af0f52d48d44bb2b2cda0fa0b4c6bb3
João Pedro Paula Pannain Souza <joaopedro@ultra.pro.br>
2018-07-03 11:39:58 -0300

    no message

---
+++ lib/models/database.ts
@@ -0,0 +1,27 @@
+import { Pool, Client } from 'pg';
+
+export class StormDatabase {
+    databaseClient: Client;
+
+
+    // constructor(host: string, user: string, password: string, database: string){
+    // }
+
+    constructor(host: string, user: string, password: string, database: string, port?: number) {
+        if (port !== undefined) port = 5432;
+
+        this.databaseClient = new Client({
+            user: user,
+            host: host,
+            database: database,
+            password: password,
+            port: port,
+        });
+    }
+
+    connect = () => {
+        this.databaseClient.connect();
+    }
+
+
+}
