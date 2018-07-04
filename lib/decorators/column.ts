0b67c8978b5464dcba62982077a26f23f37481c5
João Pedro Paula Pannain Souza <joaopedro@ultra.pro.br>
2018-07-04 00:51:18 -0300

    Update source

--- lib/decorators/column.ts
+++ lib/decorators/column.ts
@@ -3,6 +3,9 @@ import {StormutilService} from '../service';
 
 export function Column(model: any, target: any){
 
+    console.log('Column - table : ', model.constructor.name.toLowerCase());
+    console.log('Column - target: ', target);
+
 	StormutilService.setColumnTable( model.constructor.name.toLowerCase() , target);
 	return this;
 }
