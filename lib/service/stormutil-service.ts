97349b7ecead2be598efd09c8baa6c19ea82e5db
João Pedro Paula Pannain Souza <joaopedro@ultra.pro.br>
2018-07-06 18:27:26 -0300

    Update source

--- lib/service/stormutil-service.ts
+++ lib/service/stormutil-service.ts
@@ -87,14 +87,15 @@ export class StormutilService {
     }
 
     static valueSqlQuery(bean: Object){
-        const tablename: string = bean.constructor.name.toLowerCase();
+        const table: any = getStormStorage()[bean.constructor.name.toLowerCase()];
 
+        console.log('valueSqlQuery - table: ', table);
         console.log('valueSqlQuery - BEAN: ', bean);
-        console.log('valueSqlQuery - COLUMNS: ', getStormStorage()[tablename].columns);
-        console.log('valueSqlQuery - FK COLUMNS: ', getStormStorage()[tablename].fkcolumns);
+        console.log('valueSqlQuery - COLUMNS: ', table.columns);
+        console.log('valueSqlQuery - FK COLUMNS: ', table.fkcolumns);
 
         const values: any = [];
-        getStormStorage()[tablename].columns.forEach( (column: any) => {
+        table.columns.forEach( (column: any) => {
             // console.log(`${column}`, bean[column]);
             if ( bean[column] === undefined ){
                 values.push( null );
@@ -103,20 +104,23 @@ export class StormutilService {
             }
         });
 
-        getStormStorage()[tablename].fkcolumns.forEach( (element: any) => {
-            console.log('valueSqlQuery - FKCOLUMNS ELEMENT: ', element);
-            const fkcolumn = element.split('.');
-            console.log('FvalueSqlQuery KCOLUMN: ', fkcolumn);
 
-            if ( bean[fkcolumn[0]] !== undefined ){
-                if ( bean[fkcolumn[0]][fkcolumn[1]] !== undefined ) {
-                    values.push( bean[fkcolumn[0]][fkcolumn[1]] );
+        if (table.fkcolumns !== undefined){
+            table.fkcolumns.forEach( (element: any) => {
+                console.log('valueSqlQuery - FKCOLUMNS ELEMENT: ', element);
+                const fkcolumn = element.split('.');
+                console.log('FvalueSqlQuery KCOLUMN: ', fkcolumn);
+
+                if ( bean[fkcolumn[0]] !== undefined ){
+                    if ( bean[fkcolumn[0]][fkcolumn[1]] !== undefined ) {
+                        values.push( bean[fkcolumn[0]][fkcolumn[1]] );
+                    }
                 }
-            }
 
-        });
+            });
+        }
 
-        console.log('insertSqlQuery - values: ', values);
+        console.log('valueSqlQuery - values: ', values);
         return values;
     }
 
